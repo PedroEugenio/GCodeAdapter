72de604cdffe9bc1adff40ac81738c830856fe75
Pedro Eugénio <p.geno_88@hotmail.com>
2019-02-28 11:15:35 +0000

    Added Servo Controller Arduino Code

--- GCodeAdaper/GUI-GCodeAdapter.py
+++ GCodeAdaper/GUI-GCodeAdapter.py
@@ -1,5 +1,5 @@
 ################################
-## DEVELOPED BY PEDRO EUGÉNIO ##
+#  DEVELOPED BY PEDRO EUGÉNIO  #
 ################################
 import tkinter as tk
 from tkinter import filedialog
@@ -7,15 +7,18 @@ from tkinter import filedialog
 # TODO
 # - INSERT THE SPECIFIC GCODE TO RUN AFTER THE LAYER CHANGE
 # - REDESIGN LAYOUT
-#__________________________________________________________
+# __________________________________________________________
 
 window = tk.Tk()
 window.title("GUI-GCode Adapter")
 window.geometry('400x500')
 
-#----Functions----
+
+# ----Functions----
 def getPath():
-    return str(filedialog.askopenfilename(title = "Select File",filetypes = (("GCode files","*.gcode"),("Text Files","*.txt"),("all files","*.*"))))
+    return str(filedialog.askopenfilename(title="Select File", filetypes=(
+    ("GCode files", "*.gcode"), ("Text Files", "*.txt"), ("all files", "*.*"))))
+
 
 def browsefunc():
     path = getPath()
@@ -23,65 +26,72 @@ def browsefunc():
     content = readFile(path)
     filename.set(path)
 
+
 def readFile(filename):
     file = open(filename, "r")
     content = file.readlines()
     file.close()
     return content
 
-def askForNameFileOut():
-    filedialog.asksaveasfilename(title = "Save File",filetypes = (("GCode files","*.gcode"),("Text Files","*.txt"),("all files","*.*")))
 
-def processFile(content, fileOut = "out.txt"):
+def askForNameFileOut():
+    filedialog.asksaveasfilename(title="Save File",
+                                 filetypes=(("GCode files", "*.gcode"), ("Text Files", "*.txt"), ("all files", "*.*")))
+
+
+def processFile(content, fileOut="out.txt"):
     counter = 0
-    lines =0
+    lines = 0
     initLayer = False
     word = "Z"
-    #Analyze the content
+    # Analyze the content
     filenameOut = str(askForNameFileOut())
     fout = open(filenameOut, "w")
-    for i,x in enumerate(content):
+    for i, x in enumerate(content):
         if lines < 37:
             print(content[i])
-        lines +=1
+        lines += 1
         if ";LAYER:0" in x:
             initLayer = True
             print("Find Initial Layer!")
         if word in x and initLayer == True:
-            counter +=1
-            content.insert(i+1,"Geno\n")
-            #print("Word Position: "+str(i))
+            counter += 1
+            content.insert(i + 1, "Geno\n")
+            # print("Word Position: "+str(i))
         fout.write(content[i])
     fout.close()
 
+
 def convertfunc():
-    contentText.insert('1.0',content)
+    contentText.insert('1.0', content)
+
 
 def savefunc():
     processFile(content)
 
-#----Labels----
+
+# ----Labels----
 title = tk.Label(window, text="GUI-GCode Adapter by Pedro Eugénio")
 title.grid(column=1, row=0)
 
 filename = tk.StringVar()
 pathlabel2 = tk.Label(window, textvariable=filename)
 pathlabel2.grid(column=1, row=1)
-    
-#----Buttons----
-loadButton = tk.Button(window,text="Load", command=browsefunc)
+
+# ----Buttons----
+loadButton = tk.Button(window, text="Load", command=browsefunc)
 loadButton.grid(column=0, row=1)
 
-convertButton = tk.Button(window,text="Convert", command=convertfunc)
+convertButton = tk.Button(window, text="Convert", command=convertfunc)
 convertButton.grid(column=0, row=2)
 
-saveButton = tk.Button(window,text="Save", command=savefunc)
+saveButton = tk.Button(window, text="Save", command=savefunc)
 saveButton.grid(column=0, row=3)
 
-#----Text----
+# ----Text----
 contentText = tk.Text(window, width=30, height=20)
 contentText.grid(column=1, row=2)
 
-#----Entries----
+# ----Entries----
 
 window.mainloop()
---
+++ ServoController/ServoController.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo servo;
+int pinSwitch = 4;
+int positionServo = 0;
+int state =0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(9);
+  pinMode(pinSwitch, INPUT_PULLUP);
+}
+
+void loop() {
+  positionServo = 0;
+  servo.write(positionServo);
+  //state = digitalRead(pinSwitch);
+  Serial.println(digitalRead(pinSwitch));
+  if(digitalRead(pinSwitch) == 0){
+    positionServo = 20;
+    servo.write(positionServo);
+    delay(1300);
+  }
+}
